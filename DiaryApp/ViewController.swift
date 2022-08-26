675a9597e7737cb5f371704e6a390de81db8b184
annie <knr9144@icloud.com>
2022-08-26 18:09:53 +0900

    Initial Commit

---
+++ DiaryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiaryApp
+//
+//  Created by 나리강 on 2022/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
